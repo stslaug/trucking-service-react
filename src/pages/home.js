3d63aa2ed39b084a2144cf7df4ca360a469cb565
Tyler Slaughter <stslaug@gmail.com>
2024-10-11 00:18:27 -0400

    Final Push of the night

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,16 +3,33 @@ import "./css/general.css"
 
 
 const Home = () => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        async function fetchCurrentUser() {
+            try {
+                const { username, userId, signInDetails } = await getCurrentUser();
+                setCurrentUser({ username, userId, signInDetails });
+            } catch (error) {
+                console.error('Error fetching current user:', error);
+            }
+        }
+
+        fetchCurrentUser();
+    }, []);
+
     return (
         <div>
-
-        <header>
-            <h1>Home</h1>
-            <p>Haul of Fame React Page</p>
-        </header>
-
-
-
+            <header>
+                <h1>Home</h1>
+                <p>Haul of Fame React Page</p>
+            </header>
+            <div>{currentUser && (
+                    <div>
+                        <h1>Welcome {currentUser.username}!</h1>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
