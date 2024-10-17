b43b168324e9f79cd024cc8ef94a0a9d92b80ae0
Grayson Whitaker <100550253+glwhitaker@users.noreply.github.com>
2024-10-17 15:23:31 -0400

    testing amplify

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,7 @@ const Home = () => {
 
             <header>
                 <h1>Welcome to Truck Driver Incentive Program!</h1>
-                <p>By: Haul of Fame</p>
+                <p>Haul of Fame</p>
             </header>
             <main>
                 <p className="home-description">The Truck Driver Incentive Program is a program by Haul of Fame that looks to reward good truck driving!</p>
